362101d41ec3bc6fd2349ccef29b9f90f8326333
Naveen4421 <navi25231@gmail.com>
2025-09-16 20:23:38 +0530

    leetcode

---
+++ gcd_strings.py
@@ -0,0 +1,14 @@
+def gcdOfStrings(self, str1, str2):
+        """
+        :type str1: str
+        :type str2: str
+        :rtype: str
+        """
+        def gcd(a,b):
+            while b:
+                a,b=b,a%b
+            return a
+        if str1+str2!=str2+str1:
+            return ""
+        ans=gcd(len(str1),len(str2))
+        return str1[:ans]
