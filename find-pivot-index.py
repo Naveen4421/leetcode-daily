b94fff2810d557f40070d62f14d72f9b6d410376
Naveen4421 <navi25231@gmail.com>
2025-10-16 21:03:29 +0530

    leetcode 724

---
+++ find-pivot-index.py
@@ -0,0 +1,13 @@
+def pivotIndex(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        total=sum(nums)
+        leftsum=0
+        for i in range(len(nums)):
+            rightsum=total-nums[i]-leftsum
+            if leftsum==rightsum:
+                return i
+            leftsum+=nums[i]
+        return -1
