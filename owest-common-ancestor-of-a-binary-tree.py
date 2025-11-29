5a47ebafe406da60918836bf8047c45a1f806ab7
Naveen4421 <navi25231@gmail.com>
2025-11-29 22:06:42 +0530

    leetcode 236

---
+++ owest-common-ancestor-of-a-binary-tree.py
@@ -0,0 +1,21 @@
+def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        if not root:
+            return None
+        # If we reach p or q, return root
+        if root == p or root == q:
+            return root
+
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        # If both sides return non-null → root is LCA
+        if left and right:
+            return root
+        # Else either one side is non-null, or both null
+        return left if left else right
