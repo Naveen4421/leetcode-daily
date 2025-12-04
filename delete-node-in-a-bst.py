a0e5dea297ef9437708cce104a851f28168ac3c8
Naveen4421 <navi25231@gmail.com>
2025-12-04 11:30:00 +0530

    leetcode 450

---
+++ delete-node-in-a-bst.py
@@ -0,0 +1,26 @@
+def deleteNode(self, root, key):
+        """
+        :type root: Optional[TreeNode]
+        :type key: int
+        :rtype: Optional[TreeNode]
+        """
+        if not root:
+            return root
+
+        if key >root.val:
+            root.right=self.deleteNode(root.right,key)
+        elif key < root.val:
+            root.left=self.deleteNode(root.left,key)
+        else:
+            if not root.left:
+                return root.right
+            elif not root.right:
+                return root.left
+
+
+            cur = root.right
+            while cur.left:
+                cur = cur.left
+            root.val=cur.val
+            root.right=self.deleteNode(root.right,root.val)
+        return root
