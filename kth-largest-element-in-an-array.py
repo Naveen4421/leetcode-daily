2c04cc3459389181021e5c76e60303427470fcbd
Naveen4421 <navi25231@gmail.com>
2025-12-21 22:38:21 +0530

    leetcode 215

---
+++ kth-largest-element-in-an-array.py
@@ -0,0 +1,8 @@
+def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        nums.sort(reverse=True)
+        return nums[k-1]
