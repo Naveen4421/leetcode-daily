8ef7f0383036cd8d43c8cc28bc92326d29df41f3
Naveen4421 <navi25231@gmail.com>
2026-01-09 20:32:45 +0530

    leetcode 136

---
+++ single-number.py
@@ -0,0 +1,10 @@
+def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        ans = 0
+        for num in nums:
+            ans ^= num
+        return ans
+
